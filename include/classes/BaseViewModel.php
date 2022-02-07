541fec36885c2fb0e18a99eff43d377434986393
linohlyn <linohlyn@gmail.com>
2022-02-07 11:51:39 -0500

    Update BaseViewModel.php

--- include/classes/BaseViewModel.php
+++ include/classes/BaseViewModel.php
@@ -43,14 +43,8 @@ class BaseViewModel {
   private function getPdoConnection() {
     try {
       $pdoOptions = [ \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION ];
-      $dbhost = $_SERVER['aavmq3tnf4knu8.ckaktwn7nhsu.us-east-2.rds.amazonaws.com'];
-      $dbport = $_SERVER['3306'];
-      $dbname = $_SERVER['userDB'];
-      $charset = 'utf8' ;
-      $username = $_SERVER['admin'];
-      $password = $_SERVER['Welcome2db'];
-
-      return new \PDO("mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";, $username, $password, $pdoOptions);
+	    
+      return new \PDO('mysql:host=localhost;dbname=userdb', 'root', '', $pdoOptions);
     } catch (\PdoException $e) {
       echo "Connection failed" . $e->getMessage();
     }
